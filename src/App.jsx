7f63e1d070a0607fd7460680830dfa626bcb7173
Abhinit Rai <abhinit1990@gmail.com>
2025-07-18 15:11:33 +0530

    base structrure set for grid

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,24 @@ function App() {
   const config = [[1,0,1],[0,0,1],[1,1,0]]
 
   return (
-    <div style={{width:"80vw", height:"70vh", border:"1px solid red"}}>
-        {config.map((row,rowIndex)=>{
-          row.map((col, colIndex)=>{
-            console.log(row, col);
-            return (
-            <div key={`${rowIndex}-${colIndex}`} style={{width:"100px", height:"100px", border:"1px solid green"}}>
-                
+    <div style={{width:"80vw", height:"70vh", }}>
+      {config.map((row, rowIndex)=>{
+        return (
+            <div key={rowIndex} style={{display:"flex"}}>
+                {row.map((col,colIndex)=>{
+                  
+                  return<div key={`${rowIndex}-${colIndex}`} 
+                  style={{
+                    width:"100px", height:"100px",border:"1px solid red", 
+                  display:"flex",alignItems:"center", justifyContent:"center", 
+                  backgroundColor:col==1?"green":"blue"
+                }}>
+                    {col}
+                    </div>
+                })}
             </div>
-            )
-          })
-        })}
+        )
+      })}
     </div>
   )
 }
