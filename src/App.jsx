bd957c713a87b1e6b0de0beeb8c365d9b875c5ad
Abhinit Rai <abhinit1990@gmail.com>
2025-07-19 12:47:13 +0530

    clicked stack stored

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,57 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 
-  const config = [[1,0,1],[0,0,1],[1,1,0]]
+  const config = [[1,0,1],[0,0,1],[1,1,0]];
+
+  const [stack, setStack] = useState({});
+
+  let gridTrueLength = config.flat().filter(e => e==1).length;
+
+  const handleClick = (eve, rowIndex, colIndex) =>{
+    
+    const newStack = structuredClone(stack);
+    let key = `${rowIndex}-${colIndex}`;
+
+    if(newStack[key] || !config[rowIndex][colIndex]){
+      return;
+    }
+    else{
+      eve.target.style.backgroundColor='purple';
+      newStack[key] = true;
+    }
+
+    setStack(newStack);
+
+  }
+
+  useEffect(()=>{
+
+      if(Object.keys(stack).length==gridTrueLength){
+       gridReversal();
+      }
+  },[stack])
+
+  function gridReversal(){
+    
+      let arr = Object.keys(stack);
+     
+
+      for(let i=arr.length-1;i>=0;i--){
+         
+       
+        console.log("post filter",arr);
+
+        //setStack(arr);
+         //clearTimeout(timerId);
+      }
+
+    
+
+  }
 
   return (
     <div style={{width:"80vw", height:"70vh", }}>
@@ -15,12 +61,10 @@ function App() {
                 {row.map((col,colIndex)=>{
                   
                   return<div key={`${rowIndex}-${colIndex}`} 
-                  style={{
-                    width:"100px", height:"100px",border:"1px solid red", 
-                  display:"flex",alignItems:"center", justifyContent:"center", 
-                  backgroundColor:col==1?"green":"blue"
-                }}>
-                    {col}
+                  className='gridBox'
+                  onClick={(eve)=>handleClick(eve,rowIndex, colIndex)}
+                  style={{ backgroundColor:col==1?"red":"yellow" }}>
+                    
                     </div>
                 })}
             </div>
